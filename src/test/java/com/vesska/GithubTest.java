c286959144222df03797066b22d558ffd0e815f1
Sergei Savich <s.savich@ipoteka.center>
2023-01-08 22:32:21 +0300

    added selenide test

---
+++ src/test/java/com/vesska/GithubTest.java
@@ -0,0 +1,23 @@
+package com.vesska;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GithubTest {
+
+    @Test
+    void checkThatSoftAssertionsHasCodeExampleForJUnit5() {
+
+        open("https://github.com/");
+        $("[data-test-selector=nav-search-input]").setValue("Selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $$(".js-repo-nav ").findBy(Condition.visible).$("[data-tab-item=i5wiki-tab]").click();
+        $(".markdown-body").find(byText("Soft assertions")).click();
+        $(".markdown-body").shouldHave(Condition.text("3. Using JUnit5 extend test class:"));
+
+    }
+}
