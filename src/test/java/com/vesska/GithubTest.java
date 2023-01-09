f9be5d1af1741b18c79e050fbb1f518fd1a056c7
Sergei Savich <s.savich@ipoteka.center>
2023-01-09 20:28:41 +0300

    SoftAssertions is searched in Pages now

--- src/test/java/com/vesska/GithubTest.java
+++ src/test/java/com/vesska/GithubTest.java
@@ -7,6 +7,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.open;
 
+
 public class GithubTest {
 
     @Test
@@ -16,7 +17,9 @@ public class GithubTest {
         $("[data-test-selector=nav-search-input]").setValue("Selenide").pressEnter();
         $$("ul.repo-list li").first().$("a").click();
         $$(".js-repo-nav ").findBy(Condition.visible).$("[data-tab-item=i5wiki-tab]").click();
-        $(".markdown-body").find(byText("Soft assertions")).click();
+        $(".js-wiki-more-pages-link").click();
+        $("#wiki-pages-box").shouldHave(Condition.text("SoftAssertions"));
+        $$(byText("SoftAssertions")).find(Condition.visible).click();
         $(".markdown-body").shouldHave(Condition.text("3. Using JUnit5 extend test class:"));
 
     }
